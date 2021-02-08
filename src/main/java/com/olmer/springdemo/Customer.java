d40b04b391e50b08f582d1920338dc995cddcdb7
Ihor Trypolskyi <olmer@example.com>
2021-02-08 15:58:24 +0100

    docker postgres configuration

---
+++ src/main/java/com/olmer/springdemo/Customer.java
@@ -0,0 +1,22 @@
+package com.olmer.springdemo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+}
--- src/main/java/com/olmer/springdemo/SpringdemoApplication.java
+++ src/main/java/com/olmer/springdemo/SpringdemoApplication.java
@@ -1,13 +1,18 @@
 package com.olmer.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class SpringdemoApplication {
+
+	@Autowired
+	private CustomerRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringdemoApplication.class, args);
@@ -15,6 +20,17 @@ public class SpringdemoApplication {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World";
+		List allCustomers = this.repository.findAll();
+//		logger.info("Number of customers: " + allCustomers.size());
+
+		Customer newCustomer = new Customer();
+//		newCustomer.setFirstName("John");
+//		newCustomer.setLastName("Doe");
+//		logger.info("Saving new customer...");
+		this.repository.save(newCustomer);
+
+		allCustomers = this.repository.findAll();
+//		logger.info("Number of customers: " + allCustomers.size());
+		return "Hello Docker World 2: " + allCustomers.size();
 	}
 }
